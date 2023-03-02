1ee833dff2042cb6d42ad6f6c638e6269c038566
Fernando Arthur <rbdart@gmail.com>
2023-03-02 14:14:10 -0300

    first commit

--- src/java1/PrimeiroPrograma.java
+++ src/java1/PrimeiroPrograma.java
@@ -7,9 +7,10 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
     	
     	Gato gato = new Gato();
-    	Livros livros = new Livros();
+    	Livros livro = new Livro();
     	
     	System.out.println(gato);
+    	System.out.println(livro);
     	/*int a = 2;
     	int b = 3;
     	System.out.println("Hello World!" + (a+b));*/
